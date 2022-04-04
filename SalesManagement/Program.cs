2f8d2266b65cf179da35cad0fb9007febc04ee99
nguyendang127 <nguyendang.ct12@gmail.com>
2022-04-04 12:34:11 +0700

    fix

--- SalesManagement/Program.cs
+++ SalesManagement/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using Azure.Identity;
+//using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
